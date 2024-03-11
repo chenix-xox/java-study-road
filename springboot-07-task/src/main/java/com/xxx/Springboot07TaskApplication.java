e4c0d7291d957d8aa04ca7dd164265ace242629c
chenix <chuge1215@163.com>
2024-03-12 03:58:19 +0800

    添加了定时任务的示例代码

--- springboot-07-task/src/main/java/com/xxx/Springboot07TaskApplication.java
+++ springboot-07-task/src/main/java/com/xxx/Springboot07TaskApplication.java
@@ -3,12 +3,17 @@ package com.xxx;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 
 @EnableAsync
+@EnableScheduling
 @SpringBootApplication
 public class Springboot07TaskApplication {
     public static void main(String[] args) {
         SpringApplication.run(Springboot07TaskApplication.class, args);
     }
+
+
 }
---
+++ springboot-07-task/src/main/java/com/xxx/service/ScheduledService.java
@@ -0,0 +1,22 @@
+package com.xxx.service;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Chenix
+ * @create 2024-03-12 1:53
+ */
+@Service
+public class ScheduledService {
+
+    @Scheduled(cron = "0,1,2 3 2 * * ? ")
+    public void hello(){
+        System.out.println("执行了定时方法！");
+    }
+
+    @Scheduled(cron ="*/6 * * * * ?")
+    public void sayHello() {
+        System.out.println("hello");
+    }
+}
