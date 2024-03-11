a773936e967bac97f42b8491d26f6d1c212de90a
chenix <chuge1215@163.com>
2024-03-11 21:42:05 +0800

    添加了‘任务’的module

---
+++ springboot-07-task/src/main/java/com/xxx/Springboot07TaskApplication.java
@@ -0,0 +1,13 @@
+package com.xxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot07TaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot07TaskApplication.class, args);
+    }
+
+}
