4cd7ccae916f82612acb282e5009dccb2bb3b599
chenix <chuge1215@163.com>
2024-03-16 04:05:24 +0800

    添加了完整的dubbo+zookeeper示例代码

--- springboot-08-dubbo-zookeeper/provider-server/src/test/java/com/xxx/ProviderServerApplicationTests.java
+++ springboot-08-dubbo-zookeeper/provider-server/src/test/java/com/xxx/ProviderServerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ProviderServerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
