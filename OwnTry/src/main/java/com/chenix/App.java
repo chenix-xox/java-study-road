ac38aeaf622183d0e4cce027a9ce0c9676019a2a
chenix <chuge1215@163.com>
2024-04-29 23:36:17 +0800

    创建了一个测试包

--- OwnTry/src/main/java/com/chenix/App.java
+++ OwnTry/src/main/java/com/chenix/App.java
@@ -7,15 +7,10 @@ import com.chenix.model.Parent;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        Parent parent = new Parent();
-        parent.setName("父类名称");
-        parent.setAge("45");
-        Child child = parent;
-        child.setChildName("你好");
-        System.out.println(child);
+        
     }
 }
