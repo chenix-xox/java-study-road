803ec5efd94aff52ade204a4eb81236b0311294c
chenix <chuge1215@163.com>
2024-05-05 22:55:24 +0800

    添加了LambdaQueryWrapper的示例代码

--- MyBatisPlusDemo/src/test/java/com/chenix/MyBatisPlusDemoApplicationTests.java
+++ MyBatisPlusDemo/src/test/java/com/chenix/MyBatisPlusDemoApplicationTests.java
@@ -1,11 +1,13 @@
 package com.chenix;
 
 import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.chenix.mapper.BooksMapper;
 import com.chenix.pojo.Books;
 import com.chenix.service.BooksService;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -29,9 +31,18 @@ class MyBatisPlusDemoApplicationTests {
         booksQueryWrapper.like("bookName","%语言%");
         // 查询所有数据
         List<Books> userList = booksMapper.selectList(booksQueryWrapper);
+
         userList.forEach(System.out::println);
         System.out.println("=============QAQ==============");
         Books byIdBook = booksService.getById(1);
         System.out.println(byIdBook);
+
+        System.out.println("==============TAT==============");
+
+        String bookName = "传";
+        LambdaQueryWrapper<Books> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.like(StringUtils.isNotBlank(bookName),Books::getBookName,bookName);
+        List<Books> books = booksMapper.selectList(lambdaQueryWrapper);
+        books.forEach(System.out::println);
     }
 }
