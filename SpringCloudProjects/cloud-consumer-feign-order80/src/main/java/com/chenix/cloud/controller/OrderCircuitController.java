f948d10299218f0e621a640d92f00c8e30ed2fb0
chenix <chuge1215@163.com>
2024-06-11 14:38:22 +0800

    [add] 服务降级实战笔记、代码++

---
+++ SpringCloudProjects/cloud-consumer-feign-order80/src/main/java/com/chenix/cloud/controller/OrderCircuitController.java
@@ -0,0 +1,32 @@
+package com.chenix.cloud.controller;
+
+import com.chenix.cloud.apis.PayFeignApi;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author chenix
+ * @date 2024.06.06 17:26
+ * @description 断路器测试controller
+ */
+@RestController
+public class OrderCircuitController {
+
+    @Resource
+    private PayFeignApi payFeignApi;
+
+    @GetMapping(value = "/feign/pay/circuit/{id}")
+    @CircuitBreaker(name = "cloud-payment-service", fallbackMethod = "myCircuitFallback")
+    public String myCircuitBreaker(@PathVariable("id") Integer id){
+        return payFeignApi.myCircuit(id);
+    }
+
+    //myCircuitFallback就是服务降级后的兜底处理方法
+    public String myCircuitFallback(Integer id,Throwable t) {
+        // 这里是容错处理逻辑，返回备用结果
+        return id + ",myCircuitFallback，系统繁忙，请稍后再试-----/(ㄒoㄒ)/~~";
+    }
+}
