81786216060a99296fcc022b4dec667bb5b8d61e
chenix <chuge1215@163.com>
2024-08-05 02:12:43 +0800

    [fix] 网关配置更新、笔记更新、获取ZonedDateTime

--- SpringCloudProjects/cloud-api-commons/src/main/java/com/chenix/cloud/Main.java
+++ SpringCloudProjects/cloud-api-commons/src/main/java/com/chenix/cloud/Main.java
@@ -1,11 +1,15 @@
 package com.chenix.cloud;
 
+import java.time.ZonedDateTime;
+
 /**
  * @author Chenix
  * @create_date 2024/3/28 22:16
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // 默认时区
+        ZonedDateTime zbj = ZonedDateTime.now();
+        System.out.println(zbj);
     }
 }
