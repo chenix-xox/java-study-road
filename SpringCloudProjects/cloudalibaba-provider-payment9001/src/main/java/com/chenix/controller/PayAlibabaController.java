988ef57b14a57fcb0628498094961e1c59a332aa
chenix <chuge1215@163.com>
2024-08-08 17:43:15 +0800

    [feat] Alibaba，基于Nacos创建新的服务提供者

---
+++ SpringCloudProjects/cloudalibaba-provider-payment9001/src/main/java/com/chenix/controller/PayAlibabaController.java
@@ -0,0 +1,20 @@
+package com.chenix.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author chenix
+ */
+@RestController
+public class PayAlibabaController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/pay/nacos/{id}")
+    public String getPayInfo(@PathVariable("id") Integer id) {
+        return "nacos registry, serverPort: " + serverPort + "\t id" + id;
+    }
+}
